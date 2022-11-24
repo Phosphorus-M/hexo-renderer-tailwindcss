bbba1c0131f9376629760a3ed7d36bf0fc9674b7
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2022-11-24 02:44:49 -0300

    refactor: add hexo as second parameter

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 var renderer = require('./lib/renderer')
 
-hexo.extend.renderer.register('css', 'css', renderer)
+hexo.extend.renderer.register('css', 'css', renderer(hexo))
+hexo.extend.renderer.register('scss', 'css', renderer(hexo))
